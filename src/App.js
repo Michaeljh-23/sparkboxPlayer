79afe90d7c44e51bbd46bcfbef26183e49cc4e0f
Michael Harfenist <hrharfenist@gmail.com>
2021-11-17 10:30:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    <div>
+      <p>Hello {this.props.name}</p>
+    </div>
+  }
+}
+export default App;
